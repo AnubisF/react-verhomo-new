73f7a577b4155425eb91cbedddb487c4503d8fb5
AnubisF <artem.vinogradov@hotmail.com>
2022-01-28 13:17:16 +0200

    add meta tags

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {Helmet} from "react-helmet";
 
 // Home pages
 import KatariinaVerhoomo from "./pages/home/KatariinaVerhoomo";
@@ -12,20 +13,28 @@ import ScrollToTop from "./helpers/ScrollToTop";
 
 function App() {
 
-  return (
-    <Router basename={"/"}>
-      <ScrollToTop>
-        <Switch>
-          <Route
-            exact
-            path={`${process.env.PUBLIC_URL}/`}
-            component={KatariinaVerhoomo}
-          />
-          <Route component={Page404} />
-        </Switch>
-      </ScrollToTop>
-    </Router>
-  );
+    return (
+        <div className="App">
+            <Router basename={"/"}>
+                <ScrollToTop>
+                    <Switch>
+                        <Route
+                            exact
+                            path={`${process.env.PUBLIC_URL}/`}
+                            component={KatariinaVerhoomo}
+                        />
+                        <Route component={Page404}/>
+                    </Switch>
+                </ScrollToTop>
+            </Router>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Ompelimo&Verhomo Katariina</title>
+                <meta name="description" content="App Description" />
+                <meta name="theme-color" content="#008f68" />
+            </Helmet>
+        </div>
+    );
 }
 
 export default App;
